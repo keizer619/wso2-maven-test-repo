a039689275e058ec504bf13682a613156d2466f5
Kasun Gajasinghe <kasunbg@gmail.com>
2019-05-13 11:03:19 +0530

    Test us-mirror script

---
+++ us-mirror-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MAVEN_REPO=/tmp/testrepo
+clean_repo() {
+  mkdir -p $MAVEN_REPO/../backup
+  [ -e $MAVEN_REPO/org/apache/maven ] && mv $MAVEN_REPO/org/apache/maven $MAVEN_REPO/../backup
+  [ -e $MAVEN_REPO/org/codehaus ] && mv $MAVEN_REPO/org/codehaus $MAVEN_REPO/../backup
+  echo deleting contents under MAVEN_REPO = $MAVEN_REPO
+  rm -r $MAVEN_REPO
+
+  mkdir -p $MAVEN_REPO
+  [ -e $MAVEN_REPO/org/apache/maven ] && \
+    mkdir -p $MAVEN_REPO/org/apache/ && cp -r $MAVEN_REPO/../backup/maven $MAVEN_REPO/org/apache/
+  [ -e $MAVEN_REPO/org/codehaus ] && cp -r $MAVEN_REPO/../backup/codehaus $MAVEN_REPO/org/
+}
+
+if [ ! -e 'wso2-maven-test-repo' ]; then
+  git clone https://github.com/kasunbg/wso2-maven-test-repo.git
+  cd wso2-maven-test-repo
+else
+  echo existing clone found at wso2-maven-test-repo. Doing a git pull instead.
+  cd wso2-maven-test-repo
+  git pull -v
+fi
+
+clean_repo
+set -e
+./mvnw clean install -Dmaven.repo.local=$MAVEN_REPO
+
+echo completed successfully.
